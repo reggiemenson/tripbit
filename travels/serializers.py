28543c6c87e2c98f450ffe233c46ff16e6194a97
Michael Adair <mjadair91@gmail.com>
2020-01-09 17:06:44 +0000

    added serializers file

--- travels/serializers.py
+++ travels/serializers.py
@@ -3,4 +3,27 @@ from django.contrib.auth import get_user_model
 from .models import Town, Trip, Badge, Group
 User = get_user_model()
 
+class UserSerializer(serializers.ModelSerializer): # This user serializer is used to populate a nested owner on a post or comment
+
+    class Meta:
+        model = User
+        fields = 
+
+
 class TripSerializer(serializers.ModelSerializer)
+
+    class Meta:
+      model = Trip
+      fields = ('id', 'name', 'start_date', 'end_date', 'user')
+
+class BadgeSerializer(serializers.ModelSerializer)
+
+    class Meta:
+      model = Badge
+      fields =('id', 'name', 'description', 'image')
+
+
+# class PopulatedTripSerializer(serializers.ModelSerializer)
+
+#     owner = UserSerializer()
+
