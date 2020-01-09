10561aedf2ecca970b10589a7cefd215241703a8
Kathrin Eichinger <kathrin801@gmail.com>
2020-01-09 13:38:52 +0000

    town fixtures from csv

--- travels/models.py
+++ travels/models.py
@@ -4,15 +4,15 @@ from django.db import models
 # Create your models here.
 
 class Town(models.Model):
-    name = models.CharField(max_length=50)
-    name_ascii = models.CharField(max_length=50)
-    lat = models.CharField(max_length=50)
-    lng = models.CharField(max_length=50)
-    country = models.CharField(max_length=50)
-    iso2 = models.CharField(max_length=2, null=True)
-    iso3 = models.CharField(max_length=3, null=True)
-    admin_name = models.CharField(max_length=50, null=True)
-    capital = models.CharField(max_length=50, null=True)
+    name = models.CharField(max_length=255)
+    name_ascii = models.CharField(max_length=255)
+    lat = models.CharField(max_length=255)
+    lng = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    iso2 = models.CharField(max_length=255, null=True)
+    iso3 = models.CharField(max_length=255, null=True)
+    admin_name = models.CharField(max_length=255, null=True)
+    capital = models.CharField(max_length=255, null=True)
     population = models.IntegerField(null=True)
 
     def __str__(self):
