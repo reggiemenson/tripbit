86552415826a0f21f803dc7b271737b485106596
Kathrin Eichinger <kathrin801@gmail.com>
2020-01-10 11:25:37 +0000

    fixed conflicts

--- travels/serializers.py
+++ travels/serializers.py
@@ -4,17 +4,6 @@ from jwt_auth.views import UserSerializer
 from .models import Town, Trip, Badge, Group
 User = get_user_model()
 
-<<<<<<< HEAD
-class UserSerializer(serializers.ModelSerializer): 
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'score', 'dexterity', 'image')
-
-#Michael - Do we want email included in fields above? My thought would be that there is no need to display it and it creates unnecessary privacy issues?
-#Michael - I also have 'first name' and 'last name' in there - Reg says this is something that Django adds and is viewable in the admin panel - but I think we'd want this info?
-
-=======
->>>>>>> development
 
 class TripSerializer(serializers.ModelSerializer):
     class Meta:
