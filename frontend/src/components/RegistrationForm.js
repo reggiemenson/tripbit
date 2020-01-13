1b05b7a8919dd7684a25bd3f306b0b867b226da6
Georg Preuss <preussg@gmail.com>
2020-01-13 18:30:04 +0000

    minor changes

--- frontend/src/components/RegistrationForm.js
+++ frontend/src/components/RegistrationForm.js
@@ -33,123 +33,121 @@ const Register = ({ toggleRegistration, toggleLogin }) => {
         setRegister({ errors: err.response.data })
       })
   }
-  
+
   return <>
     {console.log(register)}
-    <div className=''>
-      <div className='container'>
-        <h1>Register</h1>
-        <div className='columns'>
-          <div className='column'>
-            <form className='form' onSubmit={handleSubmit}>
+    <div className='container'>
+      <h1>Register</h1>
+      <div className='columns'>
+        <div className='column'>
+          <form className='form' onSubmit={handleSubmit}>
 
-              <div className='field'>
-                <label htmlFor='username' className='label'>
-                  Username
-                </label>
-                <div className='control'>
-                  <input
-                    onChange={handleChange}
-                    className="input has-text-info"
-                    type='text'
-                    name='username'
-                  />
-                </div>
-                {register.errors.username && <small className='help is-danger'>
-                  {register.errors.username[0]}
-                </small>}
+            <div className='field'>
+              <label htmlFor='username' className='label'>
+                Username
+              </label>
+              <div className='control'>
+                <input
+                  onChange={handleChange}
+                  className="input has-text-info"
+                  type='text'
+                  name='username'
+                />
               </div>
+              {register.errors.username && <small className='help is-danger'>
+                {register.errors.username[0]}
+              </small>}
+            </div>
 
-              <div className='field'>
-                <label htmlFor='first_name' className='label'>
-                  First name
-                </label>
-                <div className='control'>
-                  <input
-                    onChange={handleChange}
-                    className="input has-text-info"
-                    type='text'
-                    name='first_name'
-                  />
-                </div>
-                {register.errors.first_name && <small className='help is-danger'>
-                  {register.errors.first_name[0]}
-                </small>}
+            <div className='field'>
+              <label htmlFor='first_name' className='label'>
+                First name
+              </label>
+              <div className='control'>
+                <input
+                  onChange={handleChange}
+                  className="input has-text-info"
+                  type='text'
+                  name='first_name'
+                />
               </div>
+              {register.errors.first_name && <small className='help is-danger'>
+                {register.errors.first_name[0]}
+              </small>}
+            </div>
 
-              <div className='field'>
-                <label htmlFor='last_name' className='label'>
-                  Last name
-                </label>
-                <div className='control'>
-                  <input
-                    onChange={handleChange}
-                    className="input has-text-info"
-                    type='text'
-                    name='last_name'
-                  />
-                </div>
-                {register.errors.last_name && <small className='help is-danger'>
-                  {register.errors.last_name[0]}
-                </small>}
+            <div className='field'>
+              <label htmlFor='last_name' className='label'>
+                Last name
+              </label>
+              <div className='control'>
+                <input
+                  onChange={handleChange}
+                  className="input has-text-info"
+                  type='text'
+                  name='last_name'
+                />
               </div>
+              {register.errors.last_name && <small className='help is-danger'>
+                {register.errors.last_name[0]}
+              </small>}
+            </div>
 
-              <div className='field'>
-                <label htmlFor='email' className='label'>
-                  Email
-                </label>
-                <div className='control'>
-                  <input
-                    onChange={handleChange}
-                    className="input has-text-info"
-                    type='email'
-                    name='email'
-                  />
-                </div>
-                {register.errors.email && <small className='help is-danger'>
-                  {register.errors.email[0]}
-                </small>}
+            <div className='field'>
+              <label htmlFor='email' className='label'>
+                Email
+              </label>
+              <div className='control'>
+                <input
+                  onChange={handleChange}
+                  className="input has-text-info"
+                  type='email'
+                  name='email'
+                />
               </div>
+              {register.errors.email && <small className='help is-danger'>
+                {register.errors.email[0]}
+              </small>}
+            </div>
 
-              <div className='field'>
-                <label htmlFor='password' className='label'>
-                  Password
-                </label>
-                <div className='control'>
-                  <input
-                    onChange={handleChange}
-                    className="input has-text-info"
-                    type='text'
-                    name='password'
-                  />
-                </div>
-                {register.errors.password && <small className='help is-danger'>
-                  {register.errors.password[0]}
-                </small>}
-
+            <div className='field'>
+              <label htmlFor='password' className='label'>
+                Password
+              </label>
+              <div className='control'>
+                <input
+                  onChange={handleChange}
+                  className="input has-text-info"
+                  type='text'
+                  name='password'
+                />
               </div>
-              <div className='field'>
-                <label htmlFor='password_confirmation' className='label'>
-                  Confirm Password
-                </label>
-                <div className='control'>
-                  <input
-                    onChange={handleChange}
-                    className="input has-text-info"
-                    type='text'
-                    name='password_confirmation'
-                  />
-                </div>
-                {register.errors.password_confirmation && <small className='help is-danger'>
-                  {register.errors.password_confirmation[0]}
-                </small>}
+              {register.errors.password && <small className='help is-danger'>
+                {register.errors.password[0]}
+              </small>}
 
+            </div>
+            <div className='field'>
+              <label htmlFor='password_confirmation' className='label'>
+                Confirm Password
+              </label>
+              <div className='control'>
+                <input
+                  onChange={handleChange}
+                  className="input has-text-info"
+                  type='text'
+                  name='password_confirmation'
+                />
               </div>
-              <button className='button is-rounded'>
-                Register
-              </button>
-            </form>
-          </div>
+              {register.errors.password_confirmation && <small className='help is-danger'>
+                {register.errors.password_confirmation[0]}
+              </small>}
+
+            </div>
+            <button className='button is-rounded'>
+              Register
+            </button>
+          </form>
         </div>
       </div>
     </div>
